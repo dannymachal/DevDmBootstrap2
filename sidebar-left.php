e83c6957f83897b66e672bc1830eadb92fec1a6b
Danny Machal <danny@devdm.com>
2013-10-20 14:49:26 +0700

    Arranging the REPO

--- sidebar-left.php
+++
@@ -1,7 +0,0 @@
-<?php
-    global $dm_settings;
-    if ($dm_settings['left_sidebar'] != 0) : ?>
-    <div class="span<?php echo $dm_settings['left_sidebar_width']; ?> dmbs-left">
-        <?php dynamic_sidebar( 'Left Sidebar' ); ?>
-    </div>
-<?php endif; ?>
--- sidebar-right.php
+++
@@ -1,8 +0,0 @@
-<?php
-    global $dm_settings;
-    if ($dm_settings['right_sidebar'] != 0) : ?>
-    <div class="span<?php echo $dm_settings['right_sidebar_width']; ?> dmbs-right">
-        <?php //get the right sidebar
-        dynamic_sidebar( 'Right Sidebar' ); ?>
-    </div>
-<?php endif; ?>
